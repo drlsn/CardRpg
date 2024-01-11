56c4546f15c2dba9f376489ed4a89ac9f4de4638
drlsn <dariusz.lusnia@gmail.com>
2024-01-11 17:43:33 +0100

    Fix canvas sizing

--- src/CardGame.Unity/Assets/Scripts/CardRPG/UseCases/DefaultCards.cs
+++ src/CardGame.Unity/Assets/Scripts/CardRPG/UseCases/DefaultCards.cs
@@ -11,24 +11,25 @@ namespace CardRPG.UseCases
             Enumerable
                 .Range(0, 14)
                 .Shuffle()
-                .Take(2)
+                .Take(12)
                 .Select((imgIdx, i) =>
                     i switch
                     {
                         0 => Zawisza(imgIdx),
                         1 => Ulryk(imgIdx),
+                        2 => Msciwoj(imgIdx),
+                        3 => Godryk(imgIdx),
+                        4 => Jerzy(imgIdx),
+                        5 => Siemko(imgIdx),
+                        6 => Jaca(imgIdx),
+                        7 => Milko(imgIdx),
+                        8 => Janek(imgIdx),
+                        9 => Kokosz(imgIdx),
+                        10 => Kajko(imgIdx),
+                        11 => Miko(imgIdx),
                         _ => Zawisza(imgIdx),
                     })
                 .ToArray();
-
-        //static DefaultCards()
-        //{
-        //    All = typeof(DefaultCards)
-        //        .GetFields(BindingFlags.Public | BindingFlags.Static)
-        //        .Select(f => f.GetValue(null))
-        //        .OfType<Card>()
-        //        .ToArray();
-        //}
 
         private static readonly Random Random = new Random();
 
@@ -47,5 +48,85 @@ namespace CardRPG.UseCases
                 hp: new StatisticPoint(10),
                 attack: new StatisticPoint(3)),
                 imgIdx);
+
+        private static Card Godryk(int imgIdx) => new Card(
+            new CardId("Godryk"),
+            "Godryk",
+            new StatisticPointGroup(
+                hp: new StatisticPoint(10),
+                attack: new StatisticPoint(3)),
+                imgIdx);
+
+        private static Card Msciwoj(int imgIdx) => new Card(
+            new CardId("Msciwoj"),
+            "Msciwoj",
+            new StatisticPointGroup(
+                hp: new StatisticPoint(10),
+                attack: new StatisticPoint(3)),
+                imgIdx);
+
+        private static Card Jerzy(int imgIdx) => new Card(
+            new CardId("Jerzy"),
+            "Jerzy",
+            new StatisticPointGroup(
+                hp: new StatisticPoint(10),
+                attack: new StatisticPoint(3)),
+                imgIdx);
+
+        private static Card Siemko(int imgIdx) => new Card(
+            new CardId("Siemko"),
+            "Siemko",
+            new StatisticPointGroup(
+                hp: new StatisticPoint(10),
+                attack: new StatisticPoint(3)),
+                imgIdx);
+
+        private static Card Jaca(int imgIdx) => new Card(
+           new CardId("Jaca"),
+           "Jaca",
+           new StatisticPointGroup(
+               hp: new StatisticPoint(10),
+               attack: new StatisticPoint(3)),
+               imgIdx);
+
+        private static Card Milko(int imgIdx) => new Card(
+           new CardId("Milko"),
+           "Milko",
+           new StatisticPointGroup(
+               hp: new StatisticPoint(10),
+               attack: new StatisticPoint(3)),
+               imgIdx);
+
+        private static Card Janek(int imgIdx) => new Card(
+           new CardId("Janek"),
+           "Janek",
+           new StatisticPointGroup(
+               hp: new StatisticPoint(10),
+               attack: new StatisticPoint(3)),
+               imgIdx);
+
+        private static Card Kokosz(int imgIdx) => new Card(
+           new CardId("Kokosz"),
+           "Kokosz",
+           new StatisticPointGroup(
+               hp: new StatisticPoint(10),
+               attack: new StatisticPoint(3)),
+               imgIdx);
+
+        private static Card Kajko(int imgIdx) => new Card(
+           new CardId("Kajko"),
+           "Kajko",
+           new StatisticPointGroup(
+               hp: new StatisticPoint(10),
+               attack: new StatisticPoint(3)),
+               imgIdx);
+
+        private static Card Miko(int imgIdx) => new Card(
+           new CardId("Miko"),
+           "Miko",
+           new StatisticPointGroup(
+               hp: new StatisticPoint(10),
+               attack: new StatisticPoint(3)),
+               imgIdx);
     }
 }
---
+++ src/CardGame.Unity/Assets/Scripts/Core/Unity/UI/RectTransformSizeExtensions.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Core.Unity.UI
+{
+    public static class RectTransformSizeExtensions
+    {
+        public static void SetWidth(this RectTransform rt, float value) =>
+            rt.sizeDelta = new(value, rt.sizeDelta.y);
+
+        public static void SetHeight(this RectTransform rt, float value) =>
+            rt.sizeDelta = new(rt.sizeDelta.x, value);
+
+        public static void SetSize(this RectTransform rt, float x, float y) =>
+            rt.sizeDelta = new(x, y);
+
+        public static void SetSize(this RectTransform rt, Vector2 value) =>
+            rt.SetSize(value.x, value.y);
+
+        public static float GetRTWidth(this Transform transform) =>
+           transform.Get<RectTransform>().rect.width;
+
+        public static float GetRTHeight(this Transform transform) =>
+            transform.Get<RectTransform>().rect.height;
+
+        public static float GetParentRTWidth(this Transform transform) =>
+            transform.parent.GetRTWidth();
+
+        public static float GetParentRTHeight(this Transform transform) =>
+            transform.parent.GetRTHeight();
+    }
+}

05dfd4f4c1959336cc0084b85d6d5eb71ea8790e
drlsn <dariusz.lusnia@gmail.com>
2024-01-26 15:08:27 +0100

    Add google play games service authentication

--- src/CardGame.Unity/Assets/GooglePlayGames/com.google.play.games/Runtime/Scripts/GameInfo.cs
+++ src/CardGame.Unity/Assets/GooglePlayGames/com.google.play.games/Runtime/Scripts/GameInfo.cs
@@ -36,10 +36,10 @@ namespace GooglePlayGames {
         private const string UnescapedWebClientId = "WEB_CLIENTID";
         private const string UnescapedNearbyServiceId = "NEARBY_SERVICE_ID";
 
-        public const string ApplicationId = ""; // Filled in automatically
+        public const string ApplicationId = "779512586139"; // Filled in automatically
         public const string IosClientId = "__IOS_CLIENTID__"; // Filled in automatically
-        public const string WebClientId = ""; // Filled in automatically
-        public const string NearbyConnectionServiceId = "";
+        public const string WebClientId = "779512586139-4es7dnjdu0jbqkor6nupdir8pseu111l.apps.googleusercontent.com"; // Filled in automatically
+        public const string NearbyConnectionServiceId = "com.netspie.trinica";
 
         public static bool ApplicationIdInitialized() {
             return !string.IsNullOrEmpty(ApplicationId) && !ApplicationId.Equals(ToEscapedToken(UnescapedApplicationId));
---
+++ src/CardGame.Unity/Assets/Scripts/Core/Unity/Auth/PlayGamesAuthentication.cs
@@ -0,0 +1,37 @@
+﻿using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using TMPro;
+using UnityEngine;
+
+namespace Core.Unity.Auth
+{
+    public class PlayGamesAuthentication : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text _text;
+
+        public void Start()
+        {
+            SignIn();
+        }
+
+        public void SignIn()
+        {
+            PlayGamesPlatform.Instance.Authenticate(ProcessAuthentication);
+        }
+
+        internal void ProcessAuthentication(SignInStatus status)
+        {
+            if (status == SignInStatus.Success)
+            {
+                var userId = PlayGamesPlatform.Instance.GetUserId();
+                var userName = PlayGamesPlatform.Instance.GetUserDisplayName();
+
+                _text.text = $"Sign In Success\nWelcome {userName}";
+            }
+            else
+            {
+                _text.text = "Sign In Failed";
+            }
+        }
+    }
+}
